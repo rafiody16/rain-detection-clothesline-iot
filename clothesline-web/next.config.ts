fe8238e7c2e413251dc4ff2f2c29411e823b4b0c
RAFI ODY PRASETYO <53549077+rafiody16@users.noreply.github.com>
2026-05-06 22:10:16 +0700

    ci: add docker image build workflow

--- clothesline-web/next.config.ts
+++ clothesline-web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   reactStrictMode: true,
+  output: "standalone",
   turbopack: {},
   images: {
     remotePatterns: [
